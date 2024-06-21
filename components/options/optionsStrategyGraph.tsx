cffa09c5bcab16dd68473f7f04f480c5b73371e7
Marvel.Codes <marvellousn561@gmail.com>
2024-06-21 12:13:02 +0100

    scaled graph

--- components/options/optionsStrategyGraph.tsx
+++ components/options/optionsStrategyGraph.tsx
@@ -2,38 +2,38 @@ import React from 'react';
 import { Svg, Line } from 'react-native-svg';
 import { ThemedView } from '../ThemedView';
 
-import { Calculated_Price_And_Profit } from "@/constants/optionstype";
+import { Calculated_Price_And_Profit } from '@/constants/optionstype';
+import { Dimensions } from "react-native";
 
 type OptionsStrategyGraphProps = {
   options: Calculated_Price_And_Profit[];
 };
 
-const OptionsStrategyGraph: React.FC<OptionsStrategyGraphProps> = ({
-  options,
-}) => {
-  // Define a range of stock prices to calculate the payoff for
-  const stockPrices = Array.from({ length: 21 }, (_, i) => i * 10); // [0, 10, 20, ..., 200]
+const OptionsStrategyGraph: React.FC<OptionsStrategyGraphProps> = ({ options }) => {
+  const { minPrice, maxPrice, minProfit, maxProfit } = findMinMax(options);
 
-  const data = Array.from({ length: 400 }, (_, i) => {
-    const top = Math.random() * 100;
-    const bottom = Math.random() * -100;
-    return i % 2 === 0 ? { x: i, y: top } : { x: i, y: bottom };
-  });
+  const getScreenSize = () => {
+    return {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    };
+  };
 
-  console.log(options)
+  const height = 200;
+  const width = getScreenSize().width > 465 ? 465 : getScreenSize().width;
 
   return (
     <ThemedView>
-      <Svg height='200' width='300'>
+      <Svg height={height} width={width} style={{backgroundColor: 'grey', marginTop: 20}}>
         {options?.map(
           (point, index) =>
             index > 0 && (
               <Line
                 key={index}
-                x1={options[index - 1].price}
-                y1={200 - options[index - 1].profit}
-                x2={point.price}
-                y2={200 - point.profit}
+                x1={normalize(options[index - 1].price, minPrice, maxPrice, width)}
+                y1={height - normalize(options[index - 1].profit, minProfit, maxProfit, height)}
+                x2={normalize(point.price, minPrice, maxPrice, width)}
+                y2={height - normalize(point.profit, minProfit, maxProfit, height)}
                 stroke='blue'
                 strokeWidth='2'
               />
@@ -42,6 +42,26 @@ const OptionsStrategyGraph: React.FC<OptionsStrategyGraphProps> = ({
       </Svg>
     </ThemedView>
   );
+};
+
+const findMinMax = (options : Calculated_Price_And_Profit[]) => {
+  let minPrice = Number.POSITIVE_INFINITY;
+  let maxPrice = Number.NEGATIVE_INFINITY;
+  let minProfit = Number.POSITIVE_INFINITY;
+  let maxProfit = Number.NEGATIVE_INFINITY;
+
+  options.forEach(option => {
+    if (option.price < minPrice) minPrice = option.price;
+    if (option.price > maxPrice) maxPrice = option.price;
+    if (option.profit < minProfit) minProfit = option.profit;
+    if (option.profit > maxProfit) maxProfit = option.profit;
+  });
+
+  return { minPrice, maxPrice, minProfit, maxProfit };
+};
+
+const normalize = (value: number, min: number, max: number, size: number) => {
+  return ((value - min) / (max - min)) * size;
 };
 
 export default OptionsStrategyGraph;
